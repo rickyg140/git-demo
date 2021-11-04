f2fb25902ebc5989074720ee458a0af3e2ec0b72
rickyg140 <55783919+rickyg140@users.noreply.github.com>
2021-11-04 10:58:19 +0100

    Update DemoApp.java

--- src/main/java/it/example/riccardo/DemoApp.java
+++ src/main/java/it/example/riccardo/DemoApp.java
@@ -5,6 +5,7 @@ public class DemoApp {
 	public static void main(String[] args) {
 		
 		System.out.println("Benvenuti nell'applicazione git-demo");
+		System.out.println("Per testare casistica che un altro sviluppatore fa delle modifiche");
 
 	}
 
